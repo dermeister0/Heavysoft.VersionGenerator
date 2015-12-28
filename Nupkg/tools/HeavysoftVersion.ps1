7e3e3213a2feb736db62b5670c69e5970e6e8a2a
Der_Meister <der@heavysoft.ru>
2015-12-29 02:04:40 +0700

    Use template-based replacement.

--- Nupkg/tools/HeavysoftVersion.ps1
+++ Nupkg/tools/HeavysoftVersion.ps1
@@ -1,52 +1,55 @@
-#-------------------------------------------------------------------------------
-# Description: Sets the AssemblyVersion and AssemblyFileVersion of 
-#              AssemblyInfo.cs files.
-# Author: Luis Rocha
-# Version: 1.0
-#-------------------------------------------------------------------------------
+# Copyright (C) 2015 Der_Meister
+# Based on original work by Luis Rocha (http://www.luisrocha.net/2009/11/setting-assembly-version-with-windows.html)
 
-$major    = 1
-$minor    = 0
-$revision = 0
+$major = 0
+$minor = 0
+$patch = 0
+$changeset = ''
 
-#-------------------------------------------------------------------------------
-# Generate a version number.
-# Note: customize this function to generate it using your version schema.
-#-------------------------------------------------------------------------------
-function Generate-VersionNumber {
-    $startDate = Get-Date -Date "2001-10-13 00:00:00Z"
-    $today = (Get-Date).ToUniversalTime()
-    $build = ($today - $startDate).Days
-    
-    return "$major.$minor.$build.$revision"
-}
- 
-#-------------------------------------------------------------------------------
-# Update version numbers of AssemblyInfo.cs
-#-------------------------------------------------------------------------------
-function Update-AssemblyInfoFiles ([string] $version) {
-    $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-    $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-    $assemblyVersion = 'AssemblyVersion("' + $version + '")';
-    $fileVersion = 'AssemblyFileVersion("' + $version + '")';
-    
-    Get-ChildItem -r -filter HeavysoftVersion.cs.template | ForEach-Object {
-        $templateName = $_.Directory.ToString() + '\' + $_.Name
-        $outputName   = $_.Directory.ToString() + '\HeavysoftVersion.cs'
-        $outputName + ' -> ' + $version
-        
-        # If you are using a source control that requires to check-out files before 
-        # modifying them, make sure to check-out the file here.
-        # For example, TFS will require the following command:
-        # tf checkout $filename
-    
-        (Get-Content $templateName) | ForEach-Object {
-            % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
-            % {$_ -replace $fileVersionPattern, $fileVersion }
-        } | Set-Content $outputName
-    }
+$logPrefix = '[Heavysoft.VersionGenerator]'
+
+if ($env:HVMajor)
+{
+    $major = $env:HVMajor
+    "$logPrefix Major: $major"
 }
 
-$version =  Generate-VersionNumber
+if ($env:HVMinor)
+{
+    $minor = $env:HVMinor
+    "$logPrefix Minor: $minor"
+}
 
-Update-AssemblyInfoFiles $version
+if ($env:HVPatch)
+{
+    $patch = $env:HVPatch
+    "$logPrefix Patch: $patch"
+}
+
+if ($env:HVChangeset)
+{
+    $changeset = $env:HVChangeset
+    "$logPrefix Changeset: $changeset"
+}
+
+$startDate = Get-Date -Date "2001-10-13 00:00:00Z"
+$today = (Get-Date).ToUniversalTime()
+$build = ($today - $startDate).Days
+"$logPrefix Build: $build"
+
+Get-ChildItem -Recurse -Filter HeavysoftVersion.cs.template | ForEach-Object {
+    $templateName = $_.Directory.ToString() + '\' + $_.Name
+    $outputName   = $_.Directory.ToString() + '\HeavysoftVersion.cs'
+
+    $lines = Get-Content $templateName | ? { $_ -NotMatch '^#.*$' }
+    $lines = (('// HeavysoftVersion.cs is auto-generated.', '// Don''t change it and add to .gitignore.') + $lines)
+    $lines | ForEach-Object {
+        % {$_ -replace '{major}', $major } |
+        % {$_ -replace '{minor}', $minor } |
+        % {$_ -replace '{patch}', $patch } |
+        % {$_ -replace '{build}', $build } |
+        % {$_ -replace '{changeset}', "-$changeset" }
+    } | Set-Content $outputName
+    
+    "$logPrefix Updated $outputName"
+}
